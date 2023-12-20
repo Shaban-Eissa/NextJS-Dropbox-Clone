9dceda3aff7c9e869187d951e5b028c349d6fc28
Shaban-Eissa <shabaneissa767@gmail.com>
2023-12-20 15:43:53 +0200

    Next14 Deploy on Vercel

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,87 @@
-import Image from 'next/image'
+import Link from "next/link";
+import { cloud, shield, smile, twinkle, arrow } from "../assets";
+import dropbox_gallery from "../assets/dropbox-clone.png";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <section className="text-white bg-[#1e1919]">
+      <div className="max-w-screen-xl px-8 py-16 mx-auto sm:px-6 lg:px-8">
+        <div className="flex flex-col items-center justify-center max-w-4xl mx-auto text-center gap-y-12">
+          <div>
+            <h2 className="text-4xl font-bold">
+              Join over 700 million registered users who trust Dropbox
+            </h2>
+            <p className="mt-4 text-lg text-gray-300">
+              Easy to use, reliable, private and secure. It&apos;s no wonder
+              Dropbox is the choice for storing and sharing your most important
+              files.
+            </p>{" "}
+          </div>
+          <Link
+            href="/dashboard"
+            className="rounded-2xl flex items-center justify-between px-6 py-6 text-lg font-bold text-gray-700 bg-blue-400 hover:opacity-75"
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            Try Dropbox Now
+            <Image src={arrow} alt="arrow" width={30} height={30} />
+          </Link>
+          <Image
+            src={dropbox_gallery}
+            alt="dropbox_gallery"
+            width={950}
+            height={600}
+          />
+
+          <h2 className="text-2xl font-bold text-center">
+            See what sets Dropbox apart
+          </h2>
+        </div>
+        <div className="grid grid-cols-2 gap-8 mt-8 md:mt-16 lg:grid-cols-4">
+          <div className="flex flex-col items-start gap-y-6">
+            <Image src={smile} alt="smile emoji" width={50} height={50} />
+            <span>Your files, not ours</span>
+            <p>
+              WIth Dropbox, your files belong to you, not us, so you can be sure
+              we&apos;re not reselling your data.
+            </p>
+          </div>
+          <div className="flex flex-col items-start gap-y-6">
+            <Image src={cloud} alt="smile emoji" width={50} height={50} />
+            <span>All your files in one place</span>
+            <p>
+              Store computer backups, photo libraries, thousands of
+              documents—all your files, in the same place.
+            </p>
+          </div>
+          <div className="flex flex-col items-start gap-y-6">
+            <Image src={twinkle} alt="smile emoji" width={50} height={50} />
+            <span>One-of-a-kind functionality</span>
+            <p>
+              Store files as large as 2 TB each, sync at light speed, and allow
+              anyone to preview nearly 200 file types.
+            </p>
+          </div>
+          <div className="flex flex-col items-start gap-y-6">
+            <Image src={shield} alt="smile emoji" width={50} height={50} />
+            <span>Ease of use and security</span>
+            <p>
+              2/3 of surveyed users say Dropbox keeps their files more secure
+              and we’re a leader in ease of use.
+            </p>
+          </div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+      <footer className="mt-20">
+        <div className="flex justify-center space-x-5">
+          <span className="font-medium text-white">Demo</span>
+          <span className="font-medium text-white">Support</span>
+          <span className="font-medium text-white">Privacy Policy</span>
+          <span className="font-medium text-white">Terms & Conditions</span>
+        </div>
+        <p className="py-10 text-center text-gray-300">
+          © 2023 Dropbox | All Rights Reserved
+        </p>
+      </footer>
+    </section>
+  );
 }
